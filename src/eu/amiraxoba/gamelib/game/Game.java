bfba7790f5848eaac20f62a025e41c7ef4abe432
AureumApes <aureumapessup@gmail.com>
2022-03-11 19:02:30 +0100

    Added Game#addRow

--- src/eu/amiraxoba/gamelib/game/Game.java
+++ src/eu/amiraxoba/gamelib/game/Game.java
@@ -5,11 +5,12 @@ import java.util.HashMap;
 
 public class Game {
     private HashMap<String, Cell> map;
+    private int height;
 
     public Game() {
         this.map = new HashMap<>();
+        height = 0;
     }
-
 
     public HashMap<String, Cell> getMap() {
         return map;
@@ -18,6 +19,17 @@ public class Game {
     public void addCell(String pos, String content){
         Cell cell = new Cell(Integer.parseInt(pos.split("")[0]), Integer.parseInt(pos.split("")[1]), content, this);
         this.map.put(pos.split("")[0] + pos.split("")[1], cell);
+        height++;
+    }
+
+    public void addRow(String row) {
+        int x = 1;
+        for(char cell : row.toCharArray()) {
+            Cell cell1 = new Cell(x, (height +1), Character.toString(cell), this);
+            this.map.put(Integer.toString(x) + (height + 1), cell1);
+            x++;
+        }
+        height++;
     }
 
     public void render(){
